c2f9f7821861c5e829fb8ac25bbc65c6f2aa46ad
master-aleksandra <aleksandra.vulevic@gmail.com>
2019-10-03 15:30:26 +0200

    Message

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$y9rm*hcf0o-!pp@pp@%em2obafi2x_qpnyo_zqa49e+(lc0m('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
 
 # Application definition
